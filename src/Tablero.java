848a532d30aa348c1554b36abb6ddef5182471eb
DanielAM31 <55641988+DanielAM31@users.noreply.github.com>
2019-09-23 04:20:46 -0500

    Se agrega regla 4 de verificacion

--- src/Tablero.java
+++ src/Tablero.java
@@ -31,6 +31,7 @@ public class Tablero {
 		table[5][5] = this.piece2.gettypepiece();
 		
 //		table[3][3] = this.piece1.gettypepiece();
+//		table[6][6] = this.piece1.gettypepiece();
 	}
 	
     //https://rstopup.com/como-puedo-copiar-una-matriz-de-2-dimensiones-en-java.html
@@ -74,7 +75,7 @@ public class Tablero {
 	}
 	
 	public void prueba(int x, int y, Piece myPiece) {
-//		int maxlenght = table.length - 1;
+		int maxlength = this.table.length - 1;
 		int[] options = new int[8];;
 		
 		Piece rivalPiece = this.piece2;
@@ -82,32 +83,30 @@ public class Tablero {
 			rivalPiece = this.piece1;
 		}
 		
-		int[] dataRule = this.ruleDir00(x, y);
+		int[] dataRule = this.ruleDir0(x, y);
 		options[0] = this.countchanges(x,y,dataRule[0],dataRule[1],dataRule[2],rivalPiece,myPiece);
+				
 		
-//		if(x != 1 && x != maxlenght && y != 1 && y != maxlenght) {
-//			options = new int[8];
-//			if(table[x-1][y-1] == pRival.gettypepiece()) {
-//				System.out.print("0.0");
-//			}
-//			if(table[x][y-1] == pRival.gettypepiece()) {
-//				System.out.print("1.0");
-//			}
-//		}
-//		else if ( (x == 1 || x == maxlenght) && (y == 1 || y == maxlenght) ) {
-//			options = new int[3];
-//		}
-//		else {
-//			options = new int[5];
-//		}
+		dataRule = this.ruleDir4(x, y, maxlength);
+		options[4] = this.countchanges(x,y,dataRule[0],dataRule[1],dataRule[2],rivalPiece,myPiece);
 	}
 	
-	private int[] ruleDir00(int x, int y) {
+	private int[] ruleDir0(int x, int y) {
 		int[] array = {-1,-1, 0};
-		int maxDiag00 = x - 1;
-		int iterar = x - y;
-		int maxIterations = maxDiag00 - iterar;
-		if (iterar < 0) {maxIterations = maxDiag00;}
+		int maxDiag = x - 1;
+		int ruleChange = x - y;
+		int maxIterations = maxDiag - ruleChange;
+		if (ruleChange < 0) {maxIterations = maxDiag;}
+		array[2] = maxIterations;
+		return array;
+	}
+	
+	private int[] ruleDir4(int x, int y, int maxlength) {
+		int[] array = {+1,+1, 0};
+		int maxDiag = maxlength - x;
+		int ruleChange = x - y;
+		int maxIterations = maxDiag;
+		if (ruleChange < 0) {maxIterations = maxDiag + ruleChange;}
 		array[2] = maxIterations;
 		return array;
 	}
@@ -119,7 +118,7 @@ public class Tablero {
 		while(index <= maxIterations){
 			x = x + xdir;
 			y = y + ydir;
-			String temp = "for " + index + "("+ y +") ";
+			String temp = "for " + index;
 			System.out.print(temp);
 			if(table[x][y] == rpiece.gettypepiece()) {
 				++counts;
